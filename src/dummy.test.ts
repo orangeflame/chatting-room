55d2d755e7d8a08d3c46d6d478f3d26d37a26534
le.zheng <le.zheng@ef.com>
2018-03-28 14:55:21 +0800

    change test into async and await

--- src/dummy.test.ts
+++ src/dummy.test.ts
@@ -1,11 +1,7 @@
 import * as request from "supertest";
 import { app } from "./app";
 
-test("dummy test", (done) => {
-  request(app)
-    .get("/health")
-    .then((res) => {
-      expect(res.status).toBe(200);
-      done();
-    });
+test("dummy test", async() => {
+  const res = await request(app).get("/health");
+  expect(res.status).toBe(200);
 });
