edd37fc5cc5803e6a91c01792acd1bd3699e2a5c
le.zheng <le.zheng@ef.com>
2018-03-28 10:32:45 +0800

    a health check

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/health", (req, res)=> res.status(200).json({
+  message: "All is well",
+}));
+
+app.listen(8080, () => {
+  console.log("dega");
+})
