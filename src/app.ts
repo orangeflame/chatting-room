6cd0ae249e264a02655089787a3d3e8e10d74a28
le.zheng <le.zheng@ef.com>
2018-03-28 17:02:52 +0800

    now we could see the aws response

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import * as express from "express";
 
 import { client } from "./modules/db/client";
+import { setup } from "./modules/db/setup";
+import { tables } from "./modules/db/tables";
 
 const app = express();
 
@@ -11,9 +13,10 @@ app.get("/health", (req, res) =>
 );
 
 app.get("/messages", async (req, res) => {
-  await client.doc.scan({ TableName: "message" }).promise();
+  await setup();
+  const data = await client.doc.scan({ TableName: tables.messages }).promise();
   res.status(200).json({
-    message: "I exist!",
+    message: data,
   });
 });
 
