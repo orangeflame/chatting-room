6f5787763cfad366df61b0fda4a0414edb700673
le.zheng <le.zheng@ef.com>
2018-03-30 14:51:18 +0800

    get more clean data in the get messages

--- src/modules/messages/get/controller.ts
+++ src/modules/messages/get/controller.ts
@@ -5,5 +5,5 @@ import { db, tables } from "modules/db";
 
 export const controller = createController(async (req, res) => {
   const data = await db.doc.scan({ TableName: tables.messages }).promise();
-  res.status(OK).json(data);
+  res.status(OK).json(data.Items);
 });
