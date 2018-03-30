5f2578201d8f99f3a2a4342e22b46595cc382c55
le.zheng <le.zheng@ef.com>
2018-03-30 16:18:16 +0800

    add change the hard coding authentication to a real one

--- src/modules/authenication/setup.ts
+++ src/modules/authenication/setup.ts
@@ -4,13 +4,20 @@ import * as passport from "passport";
 import { BasicStrategy } from "passport-http";
 
 import { authenicate } from "modules/authenication";
+import { db, tables } from "modules/db";
 
 export const setup = (app: Express) => {
   app.use(passport.initialize());
   passport.use(
-    new BasicStrategy((username, password, done) => {
-      if (username === "le" && password === "le") {
-        return done(null, {});
+    new BasicStrategy(async (username, password, done) => {
+      const { Items: users } = await db.doc
+        .scan({
+          TableName: tables.users,
+        })
+        .promise();
+      const user = users!.find((it) => it.username === username && it.password === password);
+      if (user) {
+        return done(null, user);
       }
       return done(null, false);
     }),
