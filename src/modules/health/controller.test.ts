79d3ee6951d3db1788bbfaf8817173041db01e60
le.zheng <le.zheng@ef.com>
2018-03-30 10:38:32 +0800

    mock setup for every single test

--- src/modules/health/controller.test.ts
+++ src/modules/health/controller.test.ts
@@ -3,8 +3,6 @@ import * as request from "supertest";
 
 import { ROUTE } from "./route";
 
-jest.mock("modules/db/setup/setup");
-
 test("dummy test", async () => {
   const res = await request(await appSetup()).get(ROUTE);
   expect(res.status).toBe(200);
