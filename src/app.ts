6f7b531e8a8af3df0288380837eb60765485476b
le.zheng <le.zheng@ef.com>
2018-03-30 11:09:09 +0800

    introduce body-parser and implement some dummy function of post messages

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import * as bodyPaser from "body-parser";
 import * as express from "express";
 
 import { setup } from "modules/db";
@@ -7,6 +8,7 @@ import { mount as mountMessage } from "modules/messages";
 export const appSetup = async () => {
   await setup();
   const app = express();
+  app.use(bodyPaser.json());
   mountHealth(app);
   mountMessage(app);
   return app;
--- src/modules/messages/post/controller.ts
+++ src/modules/messages/post/controller.ts
@@ -1,7 +1,9 @@
+import { logger } from "modules/core";
 import { createController } from "modules/core";
 import { db, tables } from "modules/db";
 
 export const controller = createController(async (req, res) => {
+  logger.info(req.body);
   const data = await db.doc.scan({ TableName: tables.messages }).promise();
   res.status(200).json(data);
 });
