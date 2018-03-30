3e823fe530424dfa679c5eccfb76343b39ffed07
le.zheng <le.zheng@ef.com>
2018-03-30 15:57:18 +0800

    implement create user logic

--- src/modules/users/controller.ts
+++ src/modules/users/controller.ts
@@ -1,11 +1,24 @@
 import { BAD_REQUEST } from "http-status-codes";
+import * as shortid from "shortid";
+
 import { createController } from "modules/core";
+import { db, tables } from "modules/db";
 
 import { isSignupRequest } from "./isSignupRequest";
 
-export const controller = createController((req, res) => {
+export const controller = createController(async (req, res) => {
   if (!isSignupRequest(req.body)) {
     return res.status(BAD_REQUEST).send("Bad request");
   }
-  res.status(200).send(":)");
+  const user = {
+    id: shortid.generate(),
+    ...req.body,
+  };
+  await db.doc
+    .put({
+      Item: user,
+      TableName: tables.users,
+    })
+    .promise();
+  res.status(200).send(user);
 });
